d76b54d7ecff6fac6ab259cd78c042b4561dc2a4
Matej Mikus <m.mikus@cid.com>
2022-02-15 15:03:45 +0100

    toggle modal

--- section 9 - javascript and css/work/shared.js
+++ section 9 - javascript and css/work/shared.js
@@ -1,3 +1,12 @@
 const backdrop = document.querySelector('.backdrop');
+const selectPlanButton = document.querySelectorAll('.plan .button');
+const modal = document.querySelector(".modal");
 
-backdrop.style.display = "block";
+selectPlanButton.forEach(x => x.addEventListener('click', () => toggleModal()));
+
+backdrop.addEventListener('click', () => toggleModal());
+
+const toggleModal = () => {
+    modal.style.display = modal.style.display == "block" ? "none" : "block";
+    backdrop.style.display = backdrop.style.display == "block" ? "none" : "block";
+};
